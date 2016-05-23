86396b274431c052f4c9ac9cf8d0b53441755855
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:58:46 +0300

    Add files via upload

---
+++ SeansEkleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Seans
+{
+    public class SeansEkleDTO
+    {
+        public int KacinciSaat { get; set; }
+        public int FilmID { get; set; }
+    }
+}
---
+++ SeansListeleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Seans
+{
+   public  class SeansListeleDTO
+    {
+        public int SeansID { get; set; }
+        public Nullable<int> FilmID { get; set; }
+        public Nullable<int> SeansSaat { get; set; }
+
+    }
+}
