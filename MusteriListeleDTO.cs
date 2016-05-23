c8e40378d61f51d34c4a7250eafd07437a0ce392
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:57:48 +0300

    Add files via upload

---
+++ MusteriListeleDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Musteri
+{
+    public class MusteriListeleDTO
+    {
+        public int MusteriID { get; set; }
+        public string MusteriAdı { get; set; }
+        public string MusteriSoyad { get; set; }
+    }
+}
