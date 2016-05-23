c49627c21a505c0226842d0a8b6bbd42be8d43a1
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:58:29 +0300

    Add files via upload

---
+++ SatisListeleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Satis
+{
+    public class SatisListeleDTO
+    {
+        public int SatisID { get; set; }
+        public Nullable<int> CalisanID { get; set; }
+        public Nullable<System.DateTime> SatisTarih { get; set; }
+        public Nullable<int> MusteriID { get; set; }
+        public Nullable<decimal> SatisFiyat { get; set; }
+    }
+}
