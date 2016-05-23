99b2e0ea7300ea908395d45e26193747fd87a52b
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:58:08 +0300

    Add files via upload

---
+++ SalonEkleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Salon
+{
+    public class SalonEkleDTO
+    {
+        public string SalonaName { get; set; }
+        public int SalonCapasity { get; set; }
+        public int SalonRowSayisi { get; set; }
+        public int SeansID { get; set; }
+    }
+}
---
+++ SalonListeleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Salon
+{
+    public class SalonListeleDTO
+    {
+        public int SalonID { get; set; }
+        public string SalonAd { get; set; }
+        public Nullable<int> SeansID { get; set; }
+        public Nullable<int> SalonKapasite { get; set; }
+        public Nullable<int> SalonSiraSayisi { get; set; }
+    
+    }
+}
