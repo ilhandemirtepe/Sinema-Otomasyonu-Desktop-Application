4d28907449ca83c5015ac6f36470c01a0757b73a
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:56:53 +0300

    Add files via upload

---
+++ CalisanEkleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Calisan
+{
+   public class CalisanEkleDTO
+    {
+        public string CalisanName { get; set; }
+        public string CalisanSurname { get; set; }
+        public string CalisanTcNumara { get; set; }
+        public string CalisanUsername { get; set; }
+        public string CalisanPassword{ get; set; }
+    }
+}
---
+++ CalisanListeleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Calisan
+{
+    public class CalisanListeleDTO
+    {
+        public int CalisanID { get; set; }
+        public string CalisanAd { get; set; }
+        public string CalisanSoyad { get; set; }
+        public string CalisanTcNo { get; set; }
+        public string CalisanKullaniciAd { get; set; }
+        public string CalisanParola { get; set; }
+    }
+}
