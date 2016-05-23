d057090b1b474c810a02cdbdbbb107d7c12364b8
ilhandemirtepe <han_soft@hotmail.com>
2016-05-23 20:57:10 +0300

    Add files via upload

---
+++ FilmEkleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Film
+{
+    public class FilmEkleDTO
+    {
+        public string FilmAdi { get; set; }
+        public int FilmTurID { get; set; }
+        public int  FilmSure { get; set; }
+        public bool YerliDurumu { get; set; }
+        public bool UcboyutluDurumu { get; set; }
+
+
+    }
+}
---
+++ FilmListeleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaOdev.DTO.Film
+{
+    public class FilmListeleDTO
+    {
+        public int FilmID { get; set; }
+        public string FilmAd { get; set; }
+        public Nullable<bool> FilmUcBoyutlumu { get; set; }
+        public Nullable<int> FilmTurID { get; set; }
+        public Nullable<int> FilmSure { get; set; }
+        public Nullable<bool> FilmYerlimi { get; set; }
+    }
+}
